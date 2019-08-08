c4a551eb159845ca0eef809d970195c31f36f897
Jonas Elfering <jonas.elfering@web.de>
2019-08-08 10:41:11 +0200

    Change example to clear cart action

---
+++ src/Storefront/Controller/ClearCartController.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Swag\StorefrontController\Storefront\Controller;
+
+use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClearCartController extends StorefrontController
+{
+    /**
+     * @var CartService
+     */
+    private $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
+    /**
+     * @Route("/cart/clear", name="frontend.checkout.clearCart", options={"seo"="false"}, methods={"GET"})
+     */
+    public function clearCart(SalesChannelContext $context)
+    {
+        $cart = $this->cartService->getCart($context->getToken(), $context);
+
+        foreach ($cart->getLineItems() as $lineItem) {
+            $this->cartService->remove($cart, $lineItem->getId(), $context);
+        }
+
+        return $this->forwardToRoute('frontend.checkout.cart.page');
+    }
+}
